052ebf781b431dcdef0c2e6fe355fe4428b4c5d0
WhatTheMahad <mahadnaveed22@gmail.com>
2024-10-18 03:45:58 +0400

    First Try

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained("whatthemahad/llama3")
+
+st.title = "Llama3 Demo"
+
+@st.cache
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained("whatthemahad/llama3")
+    model = AutoModelForCausalLM.from_pretrained("whatthemahad/llama3")
+    return tokenizer, model
+
+tokenizer, model = load_model()
+
